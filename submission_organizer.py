1132eadffb273fa0e40668b3e8ef850a7de26c7b
Joshua Redding <jmichael652@gmail.com>
2018-09-29 13:13:41 -0400

    Adds creation, checking, and reading of .config file

--- submission_organizer.py
+++ submission_organizer.py
@@ -8,22 +8,160 @@
 #   lastfirstmiddle_{number}_{number}_submissionname.zip
 #       this is the actual student submission zip, renamed with the prefix
 
+import os
+import sys
 
 # Setup initial config variables
-download_path = "~/Downloads"
+download_path = ''
 course_prefix = "CS-2261-A-"
 submission_suffix = "_submissions.zip"
-dest_directory = "~/Documents/GT/CS2261/ToGrade"
-
+tograde_path = '' 
+basepath = os.getcwd()
+students = []
 
 # Give welcome message
+print ("\n================================================================="
+        "===============")
 print "Welcome to the CS 2261 Canvas Student Submission Organizer!"
 print "\t...searching for config folder"
-# TODO If there is no "./config" directory
-print "\t\tNone found."
-print "\t...creating config folder"
-# TODO Create the config directory "./config"
-# TODO Create the empty student file "./config/students.txt"
+
+def test_config():
+    """Return whether a config folder exists in a completed state.
+    """
+
+    if not os.path.exists(basepath + "/.config"):
+        print "\t\tNone found."
+        return False
+    
+    if not os.path.exists(basepath + "/.config/students.txt"):
+        print "\t\tNo student file found in config folder."
+        return False
+    
+    if not os.path.exists(basepath + "/.config/paths.txt"):
+        print "\t\tNo paths file found in config folder."
+        return False
+
+    pathfile = open(basepath + "/.config/paths.txt", 'r')
+    paths = pathfile.read()
+    pathfile.close()
+    print "paths: ", paths
+
+    paths = paths.split('\n')
+    print "paths split1: ", paths
+    if len(paths) < 2:
+        print paths
+        print "\t\tNo valid paths file found in config folder."
+        return False
+    paths = paths[0].split('=') + paths[1].split('=')
+    print "paths split2: ", paths
+    if len(paths) < 4 or \
+            paths[0] != "DOWNLOAD_PATH" or \
+            paths[2] != "TOGRADE_PATH" or \
+            not os.path.exists(paths[1]) or \
+            not os.path.exists(paths[3]):
+        print '1:', paths[1], '2:', paths[3]
+        print "\t\tNo valid paths file found in config folder."
+        return False
+
+    # No problems with config, so return True
+    return True
+
+def init_config():
+    """Initialize a new config folder.
+    Called if a valid one was not found at startup.
+    """
+    
+    # Create the config folder
+    if not os.path.exists(basepath + "/.config"):
+        print "\t...creating new config folder"
+        os.mkdir(basepath + "/.config")
+
+    # Create the students file (will be populated later)
+    open(basepath + "/.config/students.txt", 'w+').close()
+
+    # Get the paths file output ready
+    newpaths = ''
+
+    # Find the download path
+    print ("\nEnter the path to your downloads folder (where you expect to find"
+           " the bulk\ndownload folder from Canvas):")
+    print "> ", # raw_input prompt not working because of flushing
+    sys.stdout.flush()
+    download_path = raw_input()
+    while not os.path.exists(download_path):
+        print ("\nThe path '%s' does not exist,\nor you do not have permission"
+                " to access it. Enter another path:") % download_path
+        print "> ",
+        sys.stdout.flush()
+        download_path = raw_input()
+    newpaths = "DOWNLOAD_PATH=" + download_path + '\n'
+
+    # Find the to-grade directory path
+    print ("\nEnter the path to the folder you want to put the assignment "
+           "folder, full of\norganized submissions:")
+    print "> ",
+    sys.stdout.flush()
+    tograde_path = raw_input()
+    while not os.path.exists(tograde_path):
+        print "\n'%s' does not exist.\nWould you like to create it? (Y/N)" % \
+            tograde_path
+        print "> ",
+        sys.stdout.flush()
+        choice = raw_input()
+        if choice == 'Y' or choice == 'y':
+            try:
+                os.mkdir(tograde_path)
+            except OSError:
+                print "\nCould not create '%s'." % tograde_path
+                choice = ''
+        else:
+            print ''  # Newline for when not 'Y', but not when OSError
+        if not (choice == 'Y' or choice == 'y'):
+            print "Enter another path:"
+            print "> ",
+            sys.stdout.flush()
+            tograde_path = raw_input()
+
+    newpaths += "TOGRADE_PATH=" + tograde_path
+
+    # Write the new paths to the file
+    pathfile = open(basepath + '/.config/paths.txt', 'w+')
+    pathfile.write(newpaths)
+    pathfile.close()
+    
+    print "\nConfig folder created.\n"
+    sys.stdout.flush()
+
+def read_config():
+    """Reads in the config files to set the script config variables.
+    Returns nothing, but sets the global variables.
+    """
+    
+    # Reference the global variables
+    global students
+    global download_path
+    global tograde_path
+    
+    # Get the students list
+    studfile = open(basepath + "/.config/students.txt", 'r')
+    students = studfile.read().split('\n')
+    studfile.close()
+
+    # Get the download path and to-grade directory
+    pathfile = open(basepath + "/.config/paths.txt", 'r')
+    paths = pathfile.read().split('\n')
+    pathfile.close()
+    paths = paths[0].split('=') + paths[1].split('=')
+    download_path = paths[1]
+    tograde_path = paths[3]
+ 
+# Check if further setup is needed, or has been completed
+if not test_config():
+    sys.stdout.flush()
+    init_config()
+read_config()
+
+# Get the submission file
 print "\t...searching for submissions in %s" % download_path
 # TODO If there is no download found
 print "\t\tNone found matching pattern '%sASSIGNMENT%s'" % \
